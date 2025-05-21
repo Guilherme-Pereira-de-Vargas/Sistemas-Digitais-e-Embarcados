b95066d152800960c7250685e3087ff30bb77266
Guilherme-Pereira-de-Vargas <guilhermepvargas79@gmail.com>
2025-05-21 12:48:19 -0300

    Atividade4.ino

--- Arduino - Semana 12/Atividade4/Atividade4.ino
+++ Arduino - Semana 12/Atividade4/Atividade4.ino
@@ -1,22 +1,49 @@
-#define pin_led1 8
-#define pin_led2 9
-#define pin_led3 10
-#define pin_botao1 3
-#define pin_botao2 2
+// Definindo os leds e botoes
+#define led1 11
+#define led2 12
+#define led3 13
 
-void setup() {
-pinMode(pin_led1, OUTPUT);
-pinMode(pin_led2, OUTPUT);
-pinMode(pin_led3, OUTPUT);
-pinMode(pin_botao1, INPUT);
-pinMode(pin_botao2, INPUT);
+#define botao1 1
+#define botao2 2
+void setup()
+{
+ pinMode(led1, OUTPUT);
+ pinMode(led2, OUTPUT);
+ pinMode(led3, OUTPUT);
+  
+ pinMode(botao1, INPUT);
+ pinMode(botao2, INPUT);
 }
 
 void loop() {
-bool estado1 = digitalRead(pin_botao1);
-bool estado2 = digitalRead(pin_botao2);
-
-if (estado1 != estado2 == 1)
-digitalWrite(pin_led1, HIGH);
-
+ // Lendo o estado dos botões
+ bool estado1 = digitalRead(botao1);
+ bool estado2 = digitalRead(botao2);
+  
+  // IF LED1
+  if (estado1 == 1 && estado2 == 1) {
+    digitalWrite(led1, HIGH);
+  }
+  
+  else {
+    digitalWrite(led1, LOW);
+  }
+  
+  // IF LED2
+  if (estado1 != estado2) {
+    digitalWrite(led2, HIGH);
+  }
+  
+  else {
+    digitalWrite(led2, LOW);
+  }
+  
+  // IF LED3
+  if (estado1 == 0 && estado2 == 0) {
+    digitalWrite(led3, HIGH);
+  }
+  
+  else {
+    digitalWrite(led3, LOW);
+  }
 }
